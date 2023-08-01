65b17149f74eb340ce234b60c5c6b39c458ad458
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-08-01 12:33:46 +0530

    [Narenthra Kishore] Add : new solution hashing to find a number

---
+++ DataStructure/Hashing/HashingOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure.Hashing
+{
+    internal class HashingOperation
+    {
+        static int size = 11;
+        LinkedList<int>[] array = new LinkedList<int>[size];
+        public HashingOperation()
+        {
+            for(int i = 0; i < size; i++)
+            {
+                array[i]=new LinkedList<int> ();
+            }
+        }
+        public void ReadFile(string filePath)
+        {
+            string readAllText = File.ReadAllText(filePath);
+            string[] words = readAllText.Split(",");
+            foreach(var data  in words)
+            {
+                int num = Convert.ToInt32(data);
+                int position = num % size;
+                array[position].Add(num);
+            }
+        }
+        public void Display()
+        {
+            int i = 0;
+            foreach (var item in array)
+            {
+                Console.WriteLine("["+(i++)+"]");
+                item.Display();
+            }
+        }
+    }
+}
--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -4,6 +4,7 @@ using DataStructure.UnorderedList;
 using System.Net.NetworkInformation;
 using DataStructure.BalancedParanthesis;
 using DataStructure.BankingCashCounter;
+using DataStructure.Hashing;
 
 namespace DataStructure
 {
@@ -12,19 +13,24 @@ namespace DataStructure
         static string unOrderedFilePath = @"E:\BridgeGateProblems\DataStructure\DataStructure\UnorderedList\UnOrderList.txt";
         static string OrderedFilePath = @"E:\BridgeGateProblems\DataStructure\DataStructure\OrderedList\OrderedList.txt";
         static string BalanceParenthesisFilepath = @"E:\BridgeGateProblems\DataStructure\DataStructure\BalancedParanthesis\BalancedParanthesisText.txt";
+        static string HashFilePath = @"E:\BridgeGateProblems\DataStructure\DataStructure\Hashing\Hash.txt";
         static void Main(string[] args)
         {
-            UnorderedListOperations operation = new UnorderedListOperations();
-            operation.ReadFileAndPerformOperation(unOrderedFilePath);
+            //UnorderedListOperations operation = new UnorderedListOperations();
+            //operation.ReadFileAndPerformOperation(unOrderedFilePath);
 
-            OrderedListOperations operation2 = new OrderedListOperations();
-            operation2.ReadAndPerformOperation(OrderedFilePath);
+            //OrderedListOperations operation2 = new OrderedListOperations();
+            //operation2.ReadAndPerformOperation(OrderedFilePath);
 
-            BalancedParanthesisOperation operation3 = new BalancedParanthesisOperation();
-            operation3.ReadAndPerformOperation(BalanceParenthesisFilepath);
+            //BalancedParanthesisOperation operation3 = new BalancedParanthesisOperation();
+            //operation3.ReadAndPerformOperation(BalanceParenthesisFilepath);
 
-            BankingOperations operations = new BankingOperations(10000);
-            operations.AddPersonsInQueue();
+            //BankingOperations operations = new BankingOperations(10000);
+            //operations.AddPersonsInQueue();
+
+            HashingOperation hashingOperation = new HashingOperation();
+            hashingOperation.ReadFile(HashFilePath);
+            hashingOperation.Display();
         }
     }
 }
